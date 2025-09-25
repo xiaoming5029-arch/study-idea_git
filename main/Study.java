d6234b583c17f4a6d46192e94f52099a27adf3ff
wangheqi <wwangheqi@163.com>
2025-09-25 21:12:12 +0800

    第5次main提交 main 删除

--- main/Study.java
+++
@@ -1,9 +0,0 @@
-public class Study {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("冲突");
-        System.out.println("main 冲突");
-        System.out.println("又冲突啦");
-    }
-}
